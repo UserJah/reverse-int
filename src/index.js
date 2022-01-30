4784129afee029e8f1355ef91970c42319d4ad10
Rustam <dzhaksimov.rustam@gmail.com>
2022-01-30 20:55:01 +0300

    feat: complete reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        return Number(String(n).split('').slice(1, String(n).length).reverse().join(''))
+    }
+    return Number(String(n).split('').reverse().join(''))
 }
